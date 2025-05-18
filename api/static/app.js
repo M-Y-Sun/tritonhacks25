de1b88352a1f1bf84d26d9154181bd5488da42ac
M-Y-Sun <sun.m.yixuan@gmail.com>
2025-05-17 19:47:58 -0700

    server edit

--- api/static/app.js
+++ api/static/app.js
@@ -11,7 +11,14 @@ async function getstream() {
     }
 }
 
-const FPS = 15;
+/** @param {ImageBitmap} bitmap */
+async function toblob(bitmap) {
+    const ocanvas = new OffscreenCanvas(bitmap.width, bitmap.height);
+    ocanvas.getContext("bitmaprenderer").transferFromImageBitmap(bitmap);
+    return await ocanvas.convertToBlob({ type: "image/png" });
+}
+
+const FPS = 1;
 
 const video = document.getElementById("video");
 const bstart = document.getElementById("bstart");
@@ -19,18 +26,41 @@ const bstop = document.getElementById("bstop");
 
 let capture, interval;
 
+var xhr = new XMLHttpRequest();
+console.log("a");
+xhr.open("POST", "https://7a96-128-54-33-224.ngrok-free.app", true);
+console.log("b");
+xhr.setRequestHeader("Content-Type", "application/octet-stream");
+// xhr.setRequestHeader("Access-Control-Allow-Origin", "http://127.0.0.1:8000");
+// xhr.setRequestHeader("Access-Control-Allow-Credentials", "true");
+console.log("c");
+
+xhr.onload = () => {
+    if (xhr.readyState == 4 && xhr.status == 201) {
+        console.log(JSON.parse(xhr.responseText));
+    } else {
+        console.log(`Error: ${xhr.status}`);
+    }
+};
+
 getstream()
     .then((stream) => {
+        video.srcObject = stream;
         bstart.onclick = () => (video.srcObject = stream);
         bstop.onclick = () => (video.srcObject = null);
 
         const track = stream.getVideoTracks()[0];
         capture = new ImageCapture(track);
+        console.log(capture);
     })
     .then(() => {
         interval = setInterval(() => {
-            capture.takePhoto().then((blob) => {
+            // capture.takePhoto(null).then((blob) => {
+            capture.grabFrame().then((bitmap) => {
+                console.log("bitmap: ", bitmap);
+                const blob = toblob(bitmap);
                 console.log("blob: ", blob);
+                xhr.send(blob);
             });
         }, 1000 / FPS);
     })
